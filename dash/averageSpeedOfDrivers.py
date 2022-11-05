e30182fcb52d8f90a56ea1c4932ce18ab6d94bd6
akhil-damarla <akhil.damarla02@gmail.com>
2022-11-05 15:22:12 -0400

    created aveage speed of drivers function

---
+++ dash/averageSpeedOfDrivers.py
@@ -0,0 +1,27 @@
+colors = {
+    "graph_bg": "#0b53c8",
+    "text": "#0b83c8",
+    "graph_line": "#107ACE"
+    }
+
+def generateAverageSpeedOfDrivers(pd, px, dcc):
+    df = pd.read_csv('/Users/akhildamarla/Desktop/FinalCleanedDataIO.csv')
+
+    df2 = df.groupby(['device'])['avgspeed_mph'].mean()
+    dfUnder = df2[df2<=25]
+    dfAvg = df2[df2<=45]
+    dfAvg = dfAvg[dfAvg>25]
+    dfOver = df2[df2>45]
+    dfFinal = [['UnderThresholdSpeed <= 25 mph',dfUnder.count() ],['BetweenThresholdSpeed > 25 mph <= 45 mph',dfAvg.count()], ['OverThresholdSpeed > 45 mph',dfOver.count() ]]
+    dffinalDF = pd.DataFrame(dfFinal, columns=['Thresholds', 'Count'])
+
+
+    fig = px.bar(dffinalDF, x = 'Thresholds', y = 'Count')
+    fig.update_layout(
+        plot_bgcolor=colors['graph_bg'],
+        font_color=colors['text']
+    )
+    return dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
